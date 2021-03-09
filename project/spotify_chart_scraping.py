ecd8dd06b1ae300f2c87c3637394e49222bbb38d
HaseU-git <hasegawayuta@MacBook-Pro-3.local>
2021-03-09 23:36:39 +0900

    fix README

---
+++ project/spotify_chart_scraping.py
@@ -0,0 +1,34 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+COUNTRY = "jp"
+URL = "https://spotifycharts.com/regional/" + COUNTRY + "/daily/"
+
+with open('date.csv', encoding = "utf-8-sig") as f:
+    reader = csv.reader(f)
+    l = [row for row in reader][0]
+    print(l)
+
+for a in l:
+	res = requests.get(URL + a)
+	soup = BeautifulSoup(res.text, 'html.parser')
+	artists = [i.get_text() for i in soup.select(".chart-table-track > span")]
+	titles = [i.get_text() for i in soup.select(".chart-table-track > strong")]
+	points = [i.get_text() for i in soup.select(".chart-table-streams")]
+	points.pop(0)
+	nums = [i+1 for i in range(200)]
+	
+	ans = []
+	#ans.append(nums)
+	ans.append(titles)
+	ans.append(artists)
+	ans.append(points)
+	print(ans)
+
+	
+	with open("datas/" + a + '.csv', 'w') as f:
+		writer = csv.writer(f)
+		writer.writerows(ans)
+	time.sleep(2)
